c733eccbfb915aa19cc64dded41d1628b8fe5bb0
Paweł Balawender <pb429141@students.mimuw.edu.pl>
2021-11-29 11:56:13 +0100

    zacznij

--- treasure.h
+++ treasure.h
@@ -6,4 +6,20 @@ concept integral = std::integral<T>;
 template<typename ValueType, bool IsTrapped>
   requires integral<ValueType>
 class Treasure {
+  ValueType value;
+
+  Treasure(ValueType value) : value(value);
+
+  ValueType evaluate() { return value; }
+
+  ValueType getLoot() { return value; value = 0; } // =0? czy to jest ok dla nieznanego typu?
+
+  bool isTrapped;
 };
+
+template<typename ValueType>
+  // requires integral<ValueType>
+class Treasure<ValueType, false> : Treasure<ValueType, false> { };
+
+using SafeTreasure = Treasure<ValueType, false>;
+using TrappedTreasure<ValueType> = Treasure<ValueType, true>;
