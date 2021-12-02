32d34593af5a99709e25a9937dbd0d1b19d75c14
Paweł Balawender <pb429141@students.mimuw.edu.pl>
2021-12-03 00:37:26 +0100

    zacznij cos

---
+++ treasure_hunt.h
@@ -0,0 +1,31 @@
+#ifndef TREASURE_HUNT_H
+#define TREASURE_HUNT_H
+
+#include <type_traits>
+#include "treasure.h"
+
+
+template<typename T>
+concept WithStaticField = requires () {
+     { [] () constexpr { return T::field; }() };
+};
+
+template <typename T>
+concept EncounterSide = requires (T t) { { T } -> Treasure; };
+
+template <typename sideA, typename sideB>
+  requires EncounterSide<sideA> && EncounterSide<sideB>
+class Encounter {
+public:
+  const sideA& a;
+  const sideB& b;
+};
+
+
+// constexpr void run(Encounter&& ... encounter) {
+
+// }
+
+// constexpr void expedition(Encounter e1, e2, ...);
+
+#endif // TREASURE_HUNT_H
