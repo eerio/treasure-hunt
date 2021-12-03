3f61615e805350b40b60e6373c6e6b6a0267728a
Paweł Balawender <pb429141@students.mimuw.edu.pl>
2021-12-03 19:29:46 +0100

    dodaj expedition()

--- treasure_hunt.h
+++ treasure_hunt.h
@@ -1,19 +1,22 @@
 #ifndef TREASURE_HUNT_H
 #define TREASURE_HUNT_H
 
+#include <concepts>
 #include <type_traits>
 #include <algorithm>
 #include "member.h"
+#include "treasure.h"
 
 template <typename T>
 concept TreasureConcept = requires (T t) {
-    { Treasure(t) } -> std::same_as<T>;
+    { Treasure{t} } -> std::same_as<T>;
 };
 
 template <typename T>
-concept MemberConcept = requires (T t) {
-    // nie wiem co tu powinno być F
-};
+concept MemberConcept = std::same_as<T, T>;
+// concept MemberConcept = requires (T t) {
+//     // { T::strength_t } -> std::integral;
+// };
 
 template<typename T>
 concept WithStaticField = requires () {
@@ -76,10 +79,13 @@ constexpr void run(Encounter<A, B> encounter) {
     encounter.b.loot(SafeTreasure(encounter.a.pay()));
 }
 
-// constexpr void run(Encounter&& ... encounter) {
 
-// }
-
-// constexpr void expedition(Encounter e1, e2, ...);
+// https://en.cppreference.com/w/cpp/language/fold
+// tam na dole jest przyklad printer() ktory robi to wlasnie tak, wiec powinno dzialac kiedys
+template <typename ...Ts>
+constexpr void expedition(Ts&&... encounters) {
+    // https://stackoverflow.com/questions/34314193/iterating-over-different-types
+    (run(encounters), ...);
+}
 
 #endif // TREASURE_HUNT_H
