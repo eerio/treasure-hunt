d46f9063fabd799c2a969e6e8780bb25de39983f
Paweł Balawender <pb429141@students.mimuw.edu.pl>
2021-12-04 14:12:03 +0100

    fix indent

--- member.h
+++ member.h
@@ -8,27 +8,32 @@
 
 constexpr std::size_t MAX_EXPEDITIONS = 25;
 
-template<typename ValueType, bool IsArmed>
-  requires integral<ValueType> 
-class Adventurer {
+template <typename ValueType, bool IsArmed>
+requires integral<ValueType>
+class Adventurer
+{
 public:
   using strength_t = uint32_t;
+
 private:
   strength_t strength = 0;
   ValueType totalLoot = 0;
+
 public:
-  constexpr Adventurer() requires (!IsArmed) = default;
+  constexpr Adventurer() requires(!IsArmed) = default;
   static constexpr const bool isArmed = IsArmed;
 
   constexpr explicit Adventurer(strength_t strength) requires IsArmed
-    : strength(strength) {}
+      : strength(strength) {}
 
   // tutaj nie mzoe byc nodiscard
   constexpr strength_t getStrength() const requires IsArmed { return this->strength; }
 
-  constexpr void loot(SafeTreasure<ValueType>&& treasure) { this->totalLoot += treasure.getLoot(); }
-  constexpr void loot(TrappedTreasure<ValueType>&& treasure) {
-    if (this->strength > 0) {
+  constexpr void loot(SafeTreasure<ValueType> &&treasure) { this->totalLoot += treasure.getLoot(); }
+  constexpr void loot(TrappedTreasure<ValueType> &&treasure)
+  {
+    if (this->strength > 0)
+    {
       this->totalLoot += treasure.getLoot();
       this->strength /= 2;
     }
@@ -47,31 +52,36 @@ public:
   //    }
   // }
 
-  constexpr ValueType pay() {
+  constexpr ValueType pay()
+  {
     ValueType temp = this->totalLoot;
     this->totalLoot = 0;
     return temp;
   }
 };
 
-template<typename ValueType>
+template <typename ValueType>
 using Explorer = Adventurer<ValueType, false>;
 
-template<typename ValueType, std::size_t CompletedExpeditions>
-  requires integral<ValueType> && (CompletedExpeditions < MAX_EXPEDITIONS)
-class Veteran {
+template <typename ValueType, std::size_t CompletedExpeditions>
+requires integral<ValueType> &&(CompletedExpeditions < MAX_EXPEDITIONS) class Veteran
+{
 public:
   using strength_t = uint32_t;
+
 private:
-  template<typename T>
-    requires integral<T>
-  static constexpr T fib(int n) { // gdy tu jest consteval zamiast constexpr to hunt_examples.cc narzeka
+  template <typename T>
+  requires integral<T>
+  static constexpr T fib(int n)
+  { // gdy tu jest consteval zamiast constexpr to hunt_examples.cc narzeka
     // static_assert(n >= 0); // to nie dziala, bo ta funkcja moze dzialac w runtime
-    if (n <= 1) return n;
-    
+    if (n <= 1)
+      return n;
+
     T pprev = 0;
     T prev = 1;
-    for (int i=0; i < n - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i)
+    {
       T temp = pprev + prev;
       pprev = prev;
       prev = temp;
@@ -87,11 +97,14 @@ public:
   constexpr Veteran() = default;
   static constexpr const bool isArmed = true;
 
-  constexpr void loot(SafeTreasure<ValueType>&& treasure) {
+  constexpr void loot(SafeTreasure<ValueType> &&treasure)
+  {
     this->totalLoot += treasure.getLoot();
   }
-  constexpr void loot(TrappedTreasure<ValueType>&& treasure) {
-    if (this->strength > 0) this->totalLoot += treasure.getLoot();
+  constexpr void loot(TrappedTreasure<ValueType> &&treasure)
+  {
+    if (this->strength > 0)
+      this->totalLoot += treasure.getLoot();
   }
   // constexpr void loot(TrappedTreasure<ValueType>&& treasure) {}
 
@@ -100,7 +113,8 @@ public:
   //     this->totalLoot += treasure.getLoot();
   // }
 
-  constexpr ValueType pay() {
+  constexpr ValueType pay()
+  {
     ValueType temp = this->totalLoot;
     this->totalLoot = 0;
     return temp;
--- treasure.h
+++ treasure.h
@@ -3,25 +3,27 @@
 
 #include <concepts>
 
-template<typename T>
+template <typename T>
 concept integral = std::integral<T>;
 
-template<typename ValueType, bool IsTrapped>
-  requires integral<ValueType>
-class Treasure {
+template <typename ValueType, bool IsTrapped>
+requires integral<ValueType>
+class Treasure
+{
   ValueType value;
 
 public:
   static constexpr bool isTrapped = IsTrapped;
 
-  constexpr explicit Treasure(ValueType value) : value(value) {};
+  constexpr explicit Treasure(ValueType value) : value(value){};
 
   constexpr ValueType evaluate() const { return this->value; }
 
-  constexpr ValueType getLoot() {
-      ValueType temp = this->value;
-      this->value = 0;
-      return temp;
+  constexpr ValueType getLoot()
+  {
+    ValueType temp = this->value;
+    this->value = 0;
+    return temp;
   }
 };
 
